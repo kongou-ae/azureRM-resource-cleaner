6b3968c7068d2414784df775f1bb6fe5f7242bc8
kongou-ae <y.matsumoto.ae@gmail.com>
2017-08-19 11:58:00 +0000

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # azureRM-resource-cleaner
+
+Shell script which deletes no-use resouces.
+
+## Notice
+
+- This script supports these resources
+    - Network Interface
+    - Public IP Address
+    - Managed Disk
+- "no-use" is that the resoure doesn't relate other resource. If you keep no releted resource, please lock the resource.
---
+++ azClean.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Get the list of locked resources
+LOCK=`az lock list --query "[].id" -o tsv`
+
+# Network Interface
+az network nic list --query "[?virtualMachine==null].id" -o tsv | while read nic
+do
+    # Check whether the resource is locked.
+    echo "$LOCK" | grep $nic -q
+    # Delete nic.
+    if test $? -eq 1 ; then
+        echo "Delete $nic"
+        az network nic delete --ids $nic
+    fi
+done
+
+# Public IP address
+az network public-ip list --query "[?ipConfiguration==null].id" -o tsv | while read pip
+do
+    # Check whether the resource is locked.
+    echo "$LOCK" | grep $pip -q
+    # Delete pip.
+    if test $? -eq 1 ; then
+        echo "Delete $pip"
+        az network public-ip delete --ids $pip
+    fi
+done
+
+# Managed Disk
+az disk list --query "[?managedBy==null].id" -o tsv | while read disk
+do
+    # Check whether the resource is locked.
+    echo "$LOCK" | grep $disk -q
+    # Delete disk.
+    if test $? -eq 1 ; then
+        echo "Delete $disk"
+        az disk delete --ids $disk --yes --no-wait
+    fi
+done
